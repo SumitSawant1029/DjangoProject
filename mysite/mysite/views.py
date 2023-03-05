2f30e0cb86168cc428477433640cf80bbae4ea65
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-03-05 21:04:11 +0530

    Comleted

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -10,12 +10,19 @@ def analyze(request):
     text = request.GET.get('text','default')
     print(text)
     removepunc = request.GET.get('removepunc','off')
-    return HttpResponse("remove punc")
+    analyzed = ""
+    puntuations = '''!()-[]{};:'"\,<>./?^#$%&*_~'''
+
+    for char in text:
+        if char not in puntuations:
+            analyzed = analyzed + char
+    param = {'purpose':'Removed Puntuations','analyzed_text':analyzed}
+    return render(request,'analyze.html',param)
 
 def index(request):
     params = {'name': 'harry','place':'USA'}
     return render(request,'index.html',params)
-    # return HttpResponse("Hello")
+
 
 # def about(request):
 #     return   HttpResponse('''<h1>Hello World</h1><a href="https://www.youtube.com/watch?v=AepgWsROO4k&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=7">Helllooo</a>"''')
