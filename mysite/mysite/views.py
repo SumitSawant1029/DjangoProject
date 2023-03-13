0502d662dd221981a1d3e362ead646cb21b44763
SumitSawant1029 <123000840+SumitSawant1029@users.noreply.github.com>
2023-03-13 20:40:38 +0530

    Final Done

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -6,16 +6,19 @@ from django.shortcuts import render
 # def return1(request):
 #     return render(request,'return.html')
 def analyze(request):
-
-    text = request.GET.get('text','default')
+    removepunc = request.POST.get('removepunc', 'off')
+    Capitalized = request.POST.get('Capitalized', 'off')
+    newline = request.POST.get('newline', 'off')
+    spaceremove = request.POST.get('spaceremove', 'off')
+    charcount = request.POST.get('charcount', 'off')
+    text = request.POST.get('text','default')
     if text == "":
         return render(request,"error.html")
+
+    elif (spaceremove != 'on' and Capitalized != 'on' and newline != 'on' and removepunc != 'on'):
+        return render(request, "error1.html")
     else:
-        removepunc = request.GET.get('removepunc','off')
-        Capitalized = request.GET.get('Capitalized','off')
-        newline = request.GET.get('newline','off')
-        spaceremove = request.GET.get('spaceremove','off')
-        charcount = request.GET.get('charcount','off')
+
         print(removepunc)
         if removepunc == "on":
     
@@ -25,45 +28,36 @@ def analyze(request):
             for char in text:
                 if char not in puntuations:
                     analyzed = analyzed + char
-            param = {'purpose':'Removed Puntuations','analyzed_text':analyzed}
-        elif newline == "on":
+            # param = {'purpose':'Removed Puntuations','analyzed_text':analyzed}
+            text = analyzed
+        if newline == "on":
             analyzed = ""
 
             for char in text:
-                if char != "\n":
+                if char != "\n" and char != '\r':
                     analyzed = analyzed + char
-            param = {'purpose':'Removed New Line Character','analyzed_text':analyzed}
+            # param = {'purpose':'Removed New Line Character','analyzed_text':analyzed}
+            text = analyzed
 
-        elif charcount == "on":
-            analyzed = ""
-            count = 0
-            for char in text:
-                if char!=" ":
-                    count = int(count) + 1
-            param = {'purpose':'Your Character Count is','analyzed_text':count}
-        elif Capitalized == "on":
+
+        if Capitalized == "on":
             analyzed = ""
 
             for char in text:
                 analyzed = analyzed + char.upper()
-            param = {'purpose':'Capitalized','analyzed_text':analyzed}
-        elif spaceremove == "on":
+            # param = {'purpose':'Capitalized','analyzed_text':analyzed}
+            text = analyzed
+        if spaceremove == "on":
             analyzed = ""
             for index,char in enumerate(text):
                 if not(text[index]==' ' and text[index+1]==' '):
                     analyzed = analyzed + char
-            param = {'purpose':'Extra Spaces removed','analyzed_text':analyzed}
-        else:
-            param = {'purpose':'Removed Puntuations','analyzed_text':text}
-        
-    return render(request,'analyze.html',param)
+            # param = {'purpose':'Extra Spaces removed','analyzed_text':analyzed}
+        param = {'purpose': 'Extra Spaces removed', 'analyzed_text': analyzed}
+        return render(request, 'analyze.html', param)
 
 def index(request):
     params = {'name': 'harry','place':'USA'}
     return render(request,'index.html',params)
 def return1(request):
     return render(request,'index.html')
-
-# def about(request):
-#     return   HttpResponse('''<h1>Hello World</h1><a href="https://www.youtube.com/watch?v=AepgWsROO4k&list=PLu0W_9lII9ah7DDtYtflgwMwpT3xmjXY9&index=7">Helllooo</a>"''')
-
